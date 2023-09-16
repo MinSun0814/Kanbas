b872a5631d9a88a419f19f77a516b42364749445
Min Sun <sun.min1@northeastern.edu>
2023-09-15 22:32:15 -0400

    Kanbas assignment 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +16,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
