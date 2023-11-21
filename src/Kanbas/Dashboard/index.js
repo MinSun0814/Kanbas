22421e9947293a712c61ba7d68ff87c45a18cb8f
Min Sun <sun.min1@northeastern.edu>
2023-11-21 02:03:25 -0500

    link

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import axios from "axios";
 
 function Dashboard({ course, setCourse }) {
-  const URL = "http://localhost:4000/api/courses";
+  const URL = "https://kanbas-node-server-a55r.onrender.com";
   const [courses, setCourses] = useState([]);
   const [newCourse, setNewCourse] = useState({ name: '', number: '', startDate: '', endDate: '' });
   const [editingCourse, setEditingCourse] = useState(null);
